198bd2e3f4c5f81926f49fd569cc93ef06ff1cab
Jenny <jenny@Jennys-MacBook-Pro.local>
2022-06-15 18:17:42 -0600

    part three

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from crypt import methods
 from flask import Flask, render_template, request, redirect
-from models import db, connect_db, User, Post
+from models import db, connect_db, User, Post, Tag, PostTag
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -78,17 +78,20 @@ def delete_user(user_id):
 
 @app.route('/users/<int:user_id>/posts/new')
 def show_new_post_form(user_id):
-    return render_template('new-post.html')
+    tags = Tag.query.all()
+    return render_template('new-post.html', tags=tags)
 
 @app.route('/users/<int:user_id>/posts/new', methods=["POST"])
 def add_new_post(user_id):
     user = User.query.get_or_404(user_id)
+    tag_ids = [int(num) for num in request.form.getlist("tags")]
+    tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
 
     user.title = request.form["title"]
     user.content = request.form["content"]
     user.id = user_id
 
-    new_post = Post(title=user.title, content=user.content, user_id=user.id)
+    new_post = Post(title=user.title, content=user.content, user_id=user.id, tags=tags)
 
     db.session.add(new_post)
     db.session.commit()
@@ -104,14 +107,18 @@ def show_post(post_id):
 @app.route('/posts/<int:post_id>/edit')
 def edit_post_form(post_id):
     post = Post.query.get(post_id)
-    return render_template('edit-post.html', post=post)
+    tags = Tag.query.all()
+    return render_template('edit-post.html', post=post, tags=tags)
 
 @app.route('/posts/<int:post_id>/edit', methods=["POST"])
 def edit_post(post_id):
     post = Post.query.get_or_404(post_id)
+    tag_ids = [int(num) for num in request.form.getlist("tags")]
+    tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
 
     post.title = request.form["title"]
     post.content = request.form["content"]
+    post.tags = tags
 
     db.session.add(post)
     db.session.commit()
@@ -124,6 +131,59 @@ def delete_post(post_id):
     db.session.commit()
 
     return redirect('/users')
+
+@app.route('/tags')
+def list_tags():
+    tags = Tag.query.all()
+    return render_template('tags.html', tags=tags)
+
+@app.route('/tags/<int:tag_id>')
+def tag_info(tag_id):
+    tag = Tag.query.get(tag_id)
+    posts = tag.posts
+    return render_template('tag-details.html', tag=tag, posts=posts)
+
+@app.route('/tags/new')
+def new_tag_form():
+    return render_template('new-tag.html')
+
+@app.route('/tags/new', methods=["POST"])
+def add_new_tag():
+    name = request.form["name"]
+
+    new_tag = Tag(name=name)
+    db.session.add(new_tag)
+    db.session.commit()
+
+    return redirect('/tags')
+
+@app.route('/tags/<int:tag_id>/edit')
+def edit_tag_form(tag_id):
+    tag = Tag.query.get(tag_id)
+
+    return render_template('edit-tag.html', tag=tag)
+
+@app.route('/tags/<int:tag_id>/edit', methods=["POST"])
+def edit_tag(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    name = request.form["name"]
+
+    tag.name = name
+    db.session.add(tag)
+    db.session.commit()
+
+    return redirect('/tags')
+
+@app.route('/tags/<int:tag_id>/delete', methods=["POST"])
+def delete_tag(tag_id):
+    PostTag.query.filter_by(tag_id=tag_id).delete()
+
+    Tag.query.filter_by(id=tag_id).delete()
+    db.session.commit()
+
+    return redirect('/tags')
+
+
 
 
 
--- models.py
+++ models.py
@@ -38,3 +38,26 @@ class Post(db.Model):
     created_at = db.Column(db.Text, nullable=False, default=datetime.now())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+
+    posts = db.relationship('Post', secondary='post_tags', backref="tags")
+
+class PostTag(db.Model):
+    __tablename__ = 'post_tags'
+
+    post_id = db.Column(db.Integer,
+                        db.ForeignKey('posts.id'),
+                        primary_key=True)
+    tag_id = db.Column(db.Integer,
+                        db.ForeignKey('tags.id'),
+                        primary_key=True)
+
+
+
